eb1775b352277a484097a5d5ea22f3d1bed900b4
Maytê <maytearaujo@yahoo.com.br>
2024-04-04 20:20:11 -0300

    Desenvolvimento da página Projetos

--- src/Components/Projetos.jsx
+++ src/Components/Projetos.jsx
@@ -1,12 +1,64 @@
 import React from "react";
-import * as S from "./Style"
+import * as S from "./Style";
+import Github from "../assets/github.png";
+import Globe from "../assets/globe.png";
+import ElasNaTech from "../assets/projetos/elas-na-tech.jpg";
+import Borcele from "../assets/projetos/Borcele.jpg";
+import Fantastika from "../assets/projetos/Fantastika.jpg";
 
-function Projetos(){
-    return(
-        <S.MainProjetos>
-            <h2>Projetos</h2>
-        </S.MainProjetos>
-    )
+function Projetos() {
+  return (
+    <S.MainProjetos>
+      <h2>Projetos</h2>
+      <S.Cards>
+        <S.Card>
+          <h3>Fantastika</h3>
+          <img src={Fantastika} alt="Fantastika" />
+          <p>
+            Projeto de conclusão do  1º módulo do curso de desenvolvimento web para mulheres realizado pelo Vai Na Web.
+          </p>
+          <S.DivSocialMediaProjetos>
+            <img src={Globe} alt="Portfolio Elas na Tech" />
+            
+            <img src={Github} alt="Projeto Github" />
+            
+          </S.DivSocialMediaProjetos>
+        </S.Card>
+
+        <S.Card>
+          <h3>Elas na tech</h3>
+          <img src={ElasNaTech} alt="Elas na Tech" />
+          <p>
+            Projeto de conclusão do programa de formação [Elas na_tech], de
+            desenvolvimento web para mulheres realizado pela Junior Achievement
+            Brasil, com o patrocínio da CCR e apoio da Skillsbuild da IBM.
+          </p>
+          <S.DivSocialMediaProjetos>
+            <img src={Globe} alt="Portfolio Elas na Tech" />
+            
+            <img src={Github} alt="Projeto Github" />
+            
+          </S.DivSocialMediaProjetos>
+        </S.Card>
+
+        <S.Card>
+          <h3>Borcele</h3>
+          <img src={Borcele} alt="Borcele" />
+          <p>
+            Projeto desenvolvido durante as aulas de React no curso de 
+            desenvolvimento web para mulheres realizado pelo Vai na Web.
+          </p>
+          <S.DivSocialMediaProjetos>
+            <img src={Globe} alt="Portfolio Elas na Tech" />
+            
+            <img src={Github} alt="Projeto Github" />
+            
+          </S.DivSocialMediaProjetos>
+        </S.Card>
+
+      </S.Cards>
+    </S.MainProjetos>
+  );
 }
 
-export default Projetos
+export default Projetos;
--- src/Components/Style.jsx
+++ src/Components/Style.jsx
@@ -2,188 +2,232 @@ import styled from "styled-components";
 
 // HEADER
 export const Nav = styled.nav`
-    background-color: #f6e6e0;
-    height: 8vh;
+  background-color: #f6e6e0;
+  height: 8vh;
 
-    display: flex;
-    justify-content: space-between;
-    text-transform: uppercase;
-    align-items: center;
+  display: flex;
+  justify-content: space-between;
+  text-transform: uppercase;
+  align-items: center;
 
-    font-size: 1.5rem;
+  font-size: 1.5rem;
 
-    a{
-        text-decoration: none;
+  a {
+    text-decoration: none;
 
-        color: #7689d8;
-        line-height: 1.5;
-        position: relative;
-    }
+    color: #7689d8;
+    line-height: 1.5;
+    position: relative;
+  }
 
-    a::before {
-    content: '';
+  a::before {
+    content: "";
     width: 0;
     height: 2px;
     border-radius: 2px;
     background-color: #7689d8;
     position: absolute;
-    bottom: -.25rem;
+    bottom: -0.25rem;
     left: 50%;
-    transition: width .4s, left .4s;
-    }
+    transition: width 0.4s, left 0.4s;
+  }
 
-    a:hover::before {
+  a:hover::before {
     width: 100%;
     left: 0;
-    }
-`
+  }
+`;
 
 export const DivLogo = styled.div`
-    display: flex;
-    justify-content: space-evenly;
-    width: 20vw;
-`
+  display: flex;
+  justify-content: space-evenly;
+  width: 20vw;
+`;
 export const DivNav = styled.div`
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
-    width: 35vw;
-    list-style-type: none;
-    
-    a{
-        text-decoration: none;
-    }
-`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 35vw;
+  list-style-type: none;
+
+  a {
+    text-decoration: none;
+  }
+`;
 
 // HOME
 export const MainHome = styled.main`
-    background-color: #7689d8;
-    height: 76vh;
-    display: flex;
-    align-items: center;
-    justify-content: left;
-    /* border: solid red; */
-`
+  background-color: #7689d8;
+  height: 76vh;
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  /* border: solid red; */
+`;
 export const SectionHome = styled.section`
-    /* border: solid yellow; */
-    display: flex;
-    justify-content: space-around;
-`
+  /* border: solid yellow; */
+  display: flex;
+  justify-content: space-around;
+`;
 export const DivSocialMediaHome = styled.div`
-    /* border: solid green; */
+  /* border: solid green; */
 
-    height: 50vh;
-    width: 6vw;
+  height: 50vh;
+  width: 6vw;
 
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-evenly; 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
 
-    img{
-        height: 3rem;
-    }
-
-    img:hover {
-  background-color: #e1b4e5;
-  border-radius: 50%;
-  transform: scale(1.1);
+  img {
+    height: 3rem;
   }
-`
+
+  img:hover {
+    background-color: #e1b4e5;
+    border-radius: 50%;
+    transform: scale(1.1);
+  }
+`;
 
 export const DivHomeLeft = styled.div`
-    /* border: solid black; */
-    width: 30vw;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
+  /* border: solid black; */
+  width: 30vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 
-    img:hover {
-  /* background-color: #f6e6e0; */
-  /* border-radius: 50%; */
-  transform: scale(1.1);
+  img:hover {
+    /* background-color: #f6e6e0; */
+    /* border-radius: 50%; */
+    transform: scale(1.1);
   }
-  
-`
+`;
 export const DivConhecimentos = styled.div`
-    /* border: solid orange; */
+  /* border: solid orange; */
 
-    width: 25vw;
-    height: 10%;
-    display: flex;
-    justify-content: space-around;
-    img{
-        width: 3rem;;
-    }
-`
+  width: 25vw;
+  height: 10%;
+  display: flex;
+  justify-content: space-around;
+  img {
+    width: 3rem;
+  }
+`;
 
 export const DivHomeRight = styled.div`
-    /* border: solid red; */
-    width: 60vw;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  /* border: solid red; */
+  width: 60vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-    img{
-        width: 25rem;
-        border-radius: 50%;
-    }
-`
-
+  img {
+    width: 25rem;
+    border-radius: 50%;
+  }
+`;
 
 // FOOTER
 export const SectionFooter = styled.footer`
-    background-color: #e1b4e5;
-    height: 16vh;
-    
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-evenly;    /* border: solid yellow; */
-    img{
-        /* width: 10vw; */
-        height: 8vh;
-    }
+  background-color: #e1b4e5;
+  height: 16vh;
 
-    img:hover {
-  background-color: #7689d8;
-  border-radius: 50%;
-  transform: scale(1.1);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly; /* border: solid yellow; */
+  img {
+    /* width: 10vw; */
+    height: 8vh;
   }
-`
+
+  img:hover {
+    background-color: #7689d8;
+    border-radius: 50%;
+    transform: scale(1.1);
+  }
+`;
 
 export const DivSocialMedia = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-    width: 10%;
-    /* border: solid blue; */
-`
+  width: 10%;
+  /* border: solid blue; */
+`;
 
 //  SOBRE
 export const MainSobre = styled.main`
-    background-color: #7689d8;
-    height: 76vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  background-color: #7689d8;
+  height: 76vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
-    p{
-        padding: 1rem;
-        
-    }
-`
-
+  p {
+    padding: 1rem;
+  }
+`;
 
 // PROJETOS
 export const MainProjetos = styled.main`
-    background-color: #7689d8;
-    height: 76vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  background-color: #7689d8;
+  height: 76vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1rem;
+  justify-content: top;
+
+`;
+
+export const Cards = styled.div`
+  /* border: solid red; */
+  
+  width: 99%;
+  height: 90%;
+  
+  display: flex;
+
+  justify-content: space-evenly;
+  /* align-items: center; */
+
+  padding: 1rem;
+`
+export const Card = styled.div`
+  /* border: solid green; */
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+ padding: .5rem;
+ 
+ img{
+  width: 80%;
+  height: 50%;
+ }
+`;
+
+export const DivSocialMediaProjetos = styled.div`
+  
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  /* border: solid pink; */
+
+   width: 40%;
+
+   padding: .6rem;
+  img {
+    height: 2.5rem;
+    width: 2.5rem;
+  }    
+  
 `
