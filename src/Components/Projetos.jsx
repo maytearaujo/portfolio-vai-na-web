c1f824135b772a8cbf3455d2959f11233a4c4168
Maytê <maytearaujo@yahoo.com.br>
2024-04-06 12:48:05 -0300

    Corrigindo projeto Borcelle com erro vercel

--- src/Components/Projetos.jsx
+++ src/Components/Projetos.jsx
@@ -3,7 +3,7 @@ import * as S from "./Style";
 import Github from "../assets/github.png";
 import Globe from "../assets/globe.png";
 
-import Borcele from "../assets/projetos/borcele.jpg";
+import Borcelle from "../assets/projetos/borcelle.jpg";
 import Fantastika from "../assets/projetos/fantastika.jpg";
 import Cartoon from "../assets/projetos/cartoon.jpg";
 
@@ -31,21 +31,18 @@ function Projetos() {
         </S.Card>
 
         <S.Card>
-          <h3>Borcele</h3>
-          <img src={Borcele} alt="Borcele" />
+          <h3>Doceria Borcelle</h3>
+          <img src={Borcelle} alt="Doceria Borcelle" />
           <h4>Desenvolvido em HTML, CSS e React.</h4>
-          <p>
-            Projeto desenvolvido durante as aulas de React no curso de 
-            desenvolvimento web para mulheres realizado pelo Vai na Web.
+          <p>Projeto desenvolvido durante as aulas de React no curso de desenvolvimento web para mulheres realizado pelo Vai na Web.
           </p>
           <S.DivSocialMediaProjetos>
             <a href="https://doceria-borcelle.vercel.app/" target="_blank">
-              <img src={Globe} alt="Padaria Borcele" />
+              <img src={Globe} alt="Doceria Borcelle" />
             </a>
             <a href="https://github.com/maytearaujo/doceria-borcelle.git" target="_blank">
               <img src={Github} alt="Projeto Github" />
-            </a>
-            
+            </a>            
           </S.DivSocialMediaProjetos>
         </S.Card>
 
