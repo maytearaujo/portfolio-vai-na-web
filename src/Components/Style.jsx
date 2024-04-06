f4cb0fd41cdc2746a7b3a82406168413dc826eac
Maytê <maytearaujo@yahoo.com.br>
2024-04-06 14:39:08 -0300

    Ajustes design Card

--- src/Components/Style.jsx
+++ src/Components/Style.jsx
@@ -235,11 +235,11 @@ export const Cards = styled.div`
   /* align-items: end; */
 
 
-  padding: 1rem;
+  padding: 1rem 0;
 
   h3 {
     font-size: 1.5rem;
-    padding-bottom: .6rem;
+    /* padding-bottom: .6rem; */
   }
 
   p, h4{
@@ -276,7 +276,7 @@ export const Card = styled.div`
 
   p{
     /* border: solid yellow; */
-    margin: 1rem;
+    margin: .8rem;
   }
 `;
 
